3e07138d26af944d5b0c7678495632a3738015a9
Samuel Zaire Rocha <96780102+szaire@users.noreply.github.com>
2023-06-22 10:44:17 -0300

    end basic CRUD implementations

--- Controllers/TaskController.cs
+++ Controllers/TaskController.cs
@@ -17,15 +17,112 @@ namespace DIO.NET._5_sistema_agendamento_tarefas.Controllers
 			_context = context;
 		}
 
+		// =========================PÁGINA INICIAL==========================
+		
         public IActionResult Index()
         {
 			var contatos = _context.Tasks.ToList<UserTask>();
 			return View(contatos);
 		}
 
+		// ===========================CRIAR TASK============================
+		
 		public IActionResult Criar()
 		{
+			return View();
+		}
+
+		// Request para criar nova tarefa
+		[HttpPost]
+		public IActionResult Criar(UserTask task)
+		{
+			if(ModelState.IsValid)
+			{
+				_context.Tasks.Add(task);
+				_context.SaveChanges();
+				return RedirectToAction(nameof(Index));
+			}
+
+			return View("Criar");
+		}
+
+		// ========================EDITAR TASK==============================
+
+		public IActionResult Editar(int id)
+		{
+			var task_db = _context.Tasks.Find(id);
+
+			if (task_db == null)
+			{
+				return NotFound();
+			}
 			
+			return View(task_db);
+		}
+
+		[HttpPost]
+		public IActionResult Editar(UserTask task)
+		{
+			var task_db = _context.Tasks.Find(task.Id);
+
+			if (task_db == null)
+			{
+				NotFound();
+			}
+
+			task_db.Titulo = task.Titulo;
+			task_db.Descricao = task.Descricao;
+			task_db.Data = task.Data;
+			task_db.Status = task.Status;
+
+			_context.Tasks.Update(task_db);
+			_context.SaveChanges();
+
+			return RedirectToAction(nameof(Index));
+		}
+
+		// ========================DETALHES TASK============================
+
+		public IActionResult Detalhes(int id)
+		{
+			var task_db = _context.Tasks.Find(id);
+
+			if (task_db == null)
+			{
+				return NotFound();
+			}
+
+			return View(task_db);
+		}
+
+		// ========================DELETAR TASK=============================
+
+		public IActionResult Deletar(int id)
+		{
+			var task_db = _context.Tasks.Find(id);
+
+			if (task_db == null)
+			{
+				return NotFound();
+			}
+
+			return View(task_db);
+		}
+
+		[HttpPost]
+		public IActionResult Deletar(UserTask task)
+		{
+			var task_db = _context.Tasks.Find(task.Id);
+			
+			if (task_db == null)
+			{
+				return NotFound();
+			}
+
+			_context.Tasks.Remove(task_db);
+			_context.SaveChanges();
+
+			return RedirectToAction(nameof(Index));
 		}
 	}
 }
